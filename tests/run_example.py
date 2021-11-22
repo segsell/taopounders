23f24bedbffc493717f3b4d60c9390ebe3497b4e
Sebastian Gsell <sebastian.gsell93@gmail.com>
2021-11-22 16:38:15 +0100

    Fix bug in example model, where delta_min and delta_max were both set to delta.

--- tests/run_example.py
+++ tests/run_example.py
@@ -22,8 +22,8 @@ nobs = exog.shape[0]
 
 # Default params
 delta = 0.1
-delta_max = 1e3
 delta_min = 1e-6
+delta_max = 1e3
 gamma0 = 0.5
 gamma1 = 2.0
 theta1 = 1e-5
@@ -32,24 +32,24 @@ eta0 = 0.0
 eta1 = 0.1
 c1 = np.sqrt(x0.shape[0])
 c2 = 10
-gnorm_sub = 1e-4
-maxiter = 50
+gtol_sub = 1e-8
+maxiter = 200
 
 solution, gradient = solve_pounders(
-    x0,
-    nobs,
-    f,
-    delta,
-    delta,
-    delta,
-    gamma0,
-    gamma1,
-    theta1,
-    theta2,
-    eta0,
-    eta1,
-    c1,
-    c2,
-    gnorm_sub,
-    maxiter,
+    x0=x0,
+    nobs=nobs,
+    criterion=f,
+    delta=delta,
+    delta_min=delta_min,
+    delta_max=delta_max,
+    gamma0=gamma0,
+    gamma1=gamma1,
+    theta1=theta1,
+    theta2=theta2,
+    eta0=eta0,
+    eta1=eta1,
+    c1=c1,
+    c2=c2,
+    gtol_sub=gtol_sub,
+    maxiter=maxiter,
 )
